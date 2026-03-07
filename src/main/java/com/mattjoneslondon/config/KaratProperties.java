812d6a2e54fa69ace15f76fdc570667a3d6bf265
Matt Jones <matthew.w.jones@me.com>
2026-03-07 11:12:16 +0000

    Reformat

--- src/main/java/com/mattjoneslondon/config/KaratProperties.java
+++ src/main/java/com/mattjoneslondon/config/KaratProperties.java
@@ -3,12 +3,7 @@ package com.mattjoneslondon.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "karat")
-public record KaratProperties(String url,
-                              String apiKey,
-                              Proxy proxy) {
-    public record Proxy(String host,
-                        int port,
-                        String username,
-                        String password) {
+public record KaratProperties(String url, String apiKey, Proxy proxy) {
+    public record Proxy(String host, int port, String username, String password) {
     }
 }
